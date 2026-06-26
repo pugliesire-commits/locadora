d8c58acb3d283147738314b07049204057538bb9
pugliesire-commits <pugliesire@gmail.com>
2026-06-26 17:54:39 -0300

    fase 21: adiciona campos de endereco e estado civil no modelo cliente

--- modelos/cliente.py
+++ modelos/cliente.py
@@ -3,13 +3,17 @@ from modelos.database import Base
 
 class Cliente(Base):
     __tablename__ = "clientes"
-
-    id       = Column(Integer, primary_key=True, index=True)
-    nome     = Column(String)
-    cpf      = Column(String, unique=True, index=True)
-    rg       = Column(String)
-    cnh      = Column(String, unique=True)
-    cnh_cat  = Column(String)
-    telefone = Column(String)
-    email    = Column(String)
-    endereco = Column(String)
+    id          = Column(Integer, primary_key=True, index=True)
+    nome        = Column(String)
+    cpf         = Column(String, unique=True, index=True)
+    rg          = Column(String)
+    cnh         = Column(String, unique=True)
+    cnh_cat     = Column(String)
+    telefone    = Column(String)
+    email       = Column(String)
+    estado_civil = Column(String)
+    endereco    = Column(String)
+    bairro      = Column(String)
+    cidade      = Column(String)
+    estado      = Column(String)
+    cep         = Column(String)
